b7f89faa586178137e49346d2ba9c4c4bc45774c
0342062207 <85429670+0342062207@users.noreply.github.com>
2022-01-18 22:07:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Gửi vợ yêu",
+    introTitle: 'Lê Khánh Thảo Chi',
+    introDesc: `Yêu em từ ngày này sang ngày khác
+    Từ tháng này sang tháng kia
+    Tình cảm anh dành cho em là bền chặt
+    Yêu em rất nhiều babi à`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Yêu em từ dòng tin nhắn đầu tiên 🥰',
+    desc: 'Em có yêu anh khum babi ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
     btnReply: 'Gửi cho bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Cùng nhau cố gắng nhé. Cứ hạnh phúc mãi thế này nhé.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://www.instagram.com/meowmeowyellowfishh/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
